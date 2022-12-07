90559a0dcb136161cc665dea26a3d7f7894e35d3
JoaoHigino <jhigino10@hotmail.com>
2022-12-07 12:00:06 +0000

    deployment commit

---
+++ envy.py
@@ -0,0 +1,4 @@
+import os
+os.environ["DATABASE_URL"] = "postgres://aampgmyi:qjBzfOIaToMzoVZ2Zpcmzd1Dq3zwxVxp@mouse.db.elephantsql.com/aampgmyi"
+os.environ["SECRET_KEY"] = "qjBzfOIaToMzoVZ2Zpcmzd1Dq3zwxVxp"
+os.environ["CLOUDINARY_URL"] = "cloudinary://596712577272615:P8kZzRjs8kVHrtSCDk-k_WTkFdA@dwqowur4a"
